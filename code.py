f441d32cd99dff9816129683817f2f0d85d2f95b
Shae Erisson <shae@scannedinavian.com>
2024-12-06 16:46:00 -0500

    port code.py from funhouse to magtag, update flowlight.el

--- code.py
+++ code.py
@@ -1,18 +1,24 @@
-# SPDX-FileCopyrightText: 2022 Dan Halbert for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
-
-from secrets import secrets  # pylint: disable=no-name-in-module
-
-from rainbowio import colorwheel
-import binascii
-import board
+import mdns
 import microcontroller
 import neopixel
 import socketpool
+import terminalio
 import wifi
-import math
-import mdns
+from adafruit_magtag.magtag import MagTag
+from adafruit_httpserver import Server, Request, Response, JSONResponse, GET
+
+magtag = MagTag()
+magtag.peripherals.neopixel_disable = False
+
+# free at start up (set to green)
+magtag.peripherals.neopixels.fill((0,0,255))
+
+magtag.add_text(
+    text_font=terminalio.FONT,
+    text_position=(140, 55),
+    text_scale=7,
+    text_anchor_point=(0.5, 0.5),
+)
 
 # from https://forums.adafruit.com/viewtopic.php?p=1016923&hilit=mdns#p1016923
 mdns_server = mdns.Server(wifi.radio)
@@ -20,49 +26,52 @@ mdns_server.hostname = "flowlight"
 print("MDNS Hostname: " + mdns_server.hostname)
 mdns_server.advertise_service(service_type="_http", protocol="_tcp", port=80)
 
-pixel_pin = board.A0 # what pin controls the neopixels?
-
-num_pixels = 30 # how many neopixels do you have?
-
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3)
-pixels.fill(0x01FF01)
-
-from adafruit_httpserver import HTTPServer, HTTPResponse
-
-ssid = secrets["ssid"]
-print("Connecting to", ssid)
-wifi.radio.connect(ssid, secrets["password"])
-print("Connected to", ssid)
-print(f"Listening on http://{wifi.radio.ipv4_address}:80")
-
 pool = socketpool.SocketPool(wifi.radio)
-server = HTTPServer(pool)
+server = Server(pool, debug=True)
 
-@server.route("/debug")
-def debug(request):
-    return HTTPResponse(status=200, body=f"{request.params}")
+@server.route("/cpu-information", append_slash=True)
+def cpu_information_handler(request: Request):
+    """
+    Return the current CPU temperature, frequency, and voltage as JSON.
+    """
+    data = {
+        "temperature": microcontroller.cpu.temperature,
+        "frequency": microcontroller.cpu.frequency,
+        "voltage": microcontroller.cpu.voltage,
+    }
+    return JSONResponse(request, data)
 
-@server.route("/brightness")
-def brightness(request):
-    new_brightness = request.params['brightness']
-    pixels.brightness = float(new_brightness)
-    return HTTPResponse(status=200, body=f"{request.params}")
+@server.route("/color", GET)
+def change_neopixel_color_handler_query_params(request: Request):
+    """Changes the color of the built-in NeoPixel using query/GET params."""
+    # e.g. /change-neopixel-color?r=255&g=0&b=0
+    r = request.query_params.get("r") or 0
+    g = request.query_params.get("g") or 0
+    b = request.query_params.get("b") or 0
 
-@server.route("/unsafe")
-def unsafe(request):
-    neopixel_values = request.params['runthis']
-    neopixel_values = binascii.a2b_base64(neopixel_values).decode('utf-8')
-    pixels[:] = eval(neopixel_values)
-    return HTTPResponse(status=200, body=f"probly worked, good luck")
+    magtag.peripherals.neopixels.fill((int(r), int(g), int(b)))
 
-# @server.route("/activity")
-# def unsafe(request):
-#     neopixel_values = request.params['activity']
-#     neopixel_values = binascii.a2b_base64(neopixel_values).decode('utf-8')
+    return Response(request, f"Changed NeoPixel to color ({r}, {g}, {b})")
 
-#     pixels[:] = eval(neopixel_values)
-#     return HTTPResponse(status=200, body=f"probly worked, good luck")
+@server.route("/status", GET)
+def set_status(request: Request):
+    """Changes the color of the built-in NeoPixels and the status text using query/GET params."""
+    # e.g. /status?status=busy or /status?status=free
+    status = request.query_params.get("status")
+    response = ""
+    if status == "busy":
+        magtag.peripherals.neopixels.fill((255, 0, 0))
+        magtag.set_text("BUSY")
+        response = "busy"
+    elif status == "free":
+        magtag.peripherals.neopixels.fill((0, 255, 0))
+        magtag.set_text("FREE")
+        response = "free"
+    else:
+        magtag.peripherals.neopixels.fill((0, 0, 255))
+        magtag.set_text("ERROR")
+        response = "error"
 
+    return Response(request, f"Changed status to {response}")
 
-# Never returns
-server.serve_forever(str(wifi.radio.ipv4_address))
+server.serve_forever(str(wifi.radio.ipv4_address),80)
--- flowlight.el
+++ flowlight.el
@@ -7,39 +7,39 @@
 
 ;;; Code:
 (require 'cl-lib)
-;; fl-activity-intervals is a list of pairs of
+;; flowlight-activity-intervals is a list of pairs of
 ;; (now-in-unix-time, emacs-idle-time-in-seconds)
-(defvar fl-activity-intervals nil)
+(defvar flowlight-activity-intervals nil)
 
 ;; this timer runs every ten seconds and adds another pair of
-;; (unix-time . emacs-idle-time) into fl-activity-intervals
+;; (unix-time . emacs-idle-time) into flowlight-activity-intervals
 
 ;; A variable that holds the timer
-(defvar fl-write-interval-timer-var nil)
+(defvar flowlight-write-interval-timer-var nil)
 
-(defun fl-write-interval ()
+(defun flowlight-write-interval ()
   "The function that writes a new pair into the variable."
   (interactive)
-  (push (cons (time-convert nil 'integer) (time-convert (current-idle-time) 'integer)) fl-activity-intervals))
+  (push (cons (time-convert nil 'integer) (time-convert (current-idle-time) 'integer)) flowlight-activity-intervals))
 
-(defun fl-write-interval-timer-start ()
+(defun flowlight-write-interval-timer-start ()
   "Start the interval timer."
   (interactive)
   ;; what does this section do for me?
-  (when (timerp fl-write-interval-timer-var)
-    (cancel-timer fl-write-interval-timer-var))
-  (setq fl-write-interval-timer-var
-	(run-with-timer 10 10 #'fl-write-interval)))
+  (when (timerp flowlight-write-interval-timer-var)
+    (cancel-timer flowlight-write-interval-timer-var))
+  (setq flowlight-write-interval-timer-var
+	(run-with-timer 10 10 #'flowlight-write-interval)))
 
-(defun fl-write-interval-timer-stop ()
+(defun flowlight-write-interval-timer-stop ()
   "Stop the interval timer."
   (interactive)
-  (when (timerp fl-write-interval-timer-var)
-    (cancel-timer fl-write-interval-timer-var)
-    (setq fl-write-interval-timer-var nil)))
+  (when (timerp flowlight-write-interval-timer-var)
+    (cancel-timer flowlight-write-interval-timer-var)
+    (setq flowlight-write-interval-timer-var nil)))
 
 ;; This time runs every thirty seconds and calculates the color to send to the LED
-(defvar fl-update-color-timer-var nil)
+(defvar flowlight-update-color-timer-var nil)
 
 
 ;; will this break if I type fast enough for (current-idle-time) to return nil ?
@@ -48,50 +48,50 @@
 idle returns -1 and active returns 1."
   (if (< (cdr pair) 10) 1 -1)) ;; 10 seconds is the sample time, best to match that?
 
-(defun fl-update-color ()
+(defun flowlight-update-color ()
   "Update the color.
 This is done by checking whether more than half of the samples
 from the last seven minutes have activity."
   (interactive)
   (progn
-    (fl-prune-activity-intervals) ;; remove old values
+    (flowlight-prune-activity-intervals) ;; remove old values
     (if
-	;; sum together all the idle and active values, if positive, send RED
-	(> (apply '+ (cl-mapcar 'is-active fl-activity-intervals)) 0)
+	;; is-active converts idle values to -1 and non-idle to +1
+	;; sum together all the idle and active values, if positive, send RED for BUSY
+	(> (apply '+ (cl-mapcar 'is-active flowlight-activity-intervals)) 0)
 	;; send RED for busy
-	(url-retrieve "http://flowlight.local/unsafe?runthis=WygyNTUsMCwwKSBmb3IgaSBpbiByYW5nZSgzMCld" 'message)
+	(url-retrieve "http://flowlight.local/status?status=busy" 'message)
       ;; send GREEN for not-busy
-      (url-retrieve "http://flowlight.local/unsafe?runthis=WygwLDI1NSwwKSBmb3IgaSBpbiByYW5nZShudW1fcGl4ZWxzKV0=" 'message))))
+      (url-retrieve "http://flowlight.local/status?status=free" 'message))))
 
-(defun fl-update-color-timer-start ()
+(defun flowlight-update-color-timer-start ()
   "Start the color update timer."
   (interactive)
   ;; what does this section do for me?
-  (when (timerp fl-update-color-timer-var)
-    (cancel-timer fl-update-color-timer-var))
-  (setq fl-update-color-timer-var
-	(run-with-timer 30 30 #'fl-update-color)))
+  (when (timerp flowlight-update-color-timer-var)
+    (cancel-timer flowlight-update-color-timer-var))
+  (setq flowlight-update-color-timer-var
+	(run-with-timer 30 30 #'flowlight-update-color)))
 
-(defun fl-update-color-timer-stop ()
+(defun flowlight-update-color-timer-stop ()
   "Stop the color update timer."
   (interactive)
-  (when (timerp fl-update-color-timer-var)
-    (cancel-timer fl-update-color-timer-var)
-    (setq fl-update-color-timer-var nil)))
+  (when (timerp flowlight-update-color-timer-var)
+    (cancel-timer flowlight-update-color-timer-var)
+    (setq flowlight-update-color-timer-var nil)))
 
 ;; this function removes values that are older than seven minutes ago
-(defun fl-prune-activity-intervals ()
+(defun flowlight-prune-activity-intervals ()
   "Remove any samples older than seven minutes."
   (interactive)
-  (let
-      ((seven-minutes-before-now (- (time-convert nil 'integer) (* 60 7))))
-    (setq fl-activity-intervals
-	  (cl-remove-if (lambda (row) (> seven-minutes-before-now (car row))) fl-activity-intervals))))
+  (let ((seven-minutes-before-now (- (time-convert nil 'integer) (* 60 7))))
+    (setq flowlight-activity-intervals
+	  (cl-remove-if (lambda (row) (> seven-minutes-before-now (car row))) flowlight-activity-intervals))))
 
 ;; start recording idle time every ten seconds!
-(fl-write-interval-timer-start)
+(flowlight-write-interval-timer-start)
 ;; start updating the activity color every thirty seconds!
-(fl-update-color-timer-start)
+(flowlight-update-color-timer-start)
 
 (provide 'flowlight)
 ;;; flowlight.el ends here
